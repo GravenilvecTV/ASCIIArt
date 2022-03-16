d1678365cd7a30a66a3a6632a9e1729672454aa3
CodeElevator <98259474+CodeElevator@users.noreply.github.com>
2022-03-16 19:28:31 +0200

    Ajout d'une fonction de telechargement d'image via un lien plus error handler

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from PIL import Image
+import requests
 
 ascii_char = ' .:-=+*#%@'  # du plus foncé au plus clair
+while True:
+    # On dit à l'utilisateur d'entrer un lien:
+    link = input("Entrez un lien d'une image: ")
+    r = requests.get(link)
+    # SI l'utilisateur a entré un faux lien:
+    if r.status_code == 404:
+        print("Le lien n'est pas disponible!")
 
-with Image.open("joconde.jpg") as image:
-    image = image.resize((500, 500))
+    # EN REVANCHE si l'utiulisateur en entre un bon: 
+    elif r.status_code == 200:
+        file = open("image.jpg", "wb")
+        file.write(response.content)
+        file.close()
 
-    for y in range(image.height):
-        line = ""
-        for x in range(image.width):
-            # on récupère le pixel sous format RGB
-            rgb = image.getpixel((x, y))
-            # on fait la moyenne de chaque channel pour faire une nuance de gris
-            grey = sum(rgb) // len(rgb)
-            # on fait un produit en croix pour récupérer l'index
-            index = grey * 9 // 255
-            # on rajoute 2 espaces, car en console la hauteur d'un char est plus grande que sa largeur
-            line += ascii_char[index] + "  "
-        print(line)
+    with Image.open("image.jpg") as image:
+        image = image.resize((500, 500))
+
+        for y in range(image.height):
+            line = ""
+            for x in range(image.width):
+                # on récupère le pixel sous format RGB
+                rgb = image.getpixel((x, y))
+                # on fait la moyenne de chaque channel pour faire une nuance de gris
+                grey = sum(rgb) // len(rgb)
+                # on fait un produit en croix pour récupérer l'index
+                index = grey * 9 // 255
+                # on rajoute 2 espaces, car en console la hauteur d'un char est plus grande que sa largeur
+                line += ascii_char[index] + "  "
+            print(line)
 
 
+# On met le tout dans une boucle while au cas où il y a une erreur.
